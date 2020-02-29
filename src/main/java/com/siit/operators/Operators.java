6337b1b615aa249f03448119691b0f23c0868787
Ionut Spalatelu <ionutspalatelu@gmail.com>
2020-02-29 10:13:43 +0200

    Examples from day 3
    Signed-off-by: Ionut Spalatelu <ionut.spalatelu@gohenry.co.uk>

---
+++ src/main/java/com/siit/operators/Operators.java
@@ -0,0 +1,26 @@
+package com.siit.operators;
+
+public class Operators {
+
+    public static void main(String... args) {
+        int contor = 5;
+//        System.out.println(++contor); //6
+//        System.out.println(contor++); //5
+//        System.out.println(contor); //6
+
+        int result = contor++; //5
+        System.out.println(contor); //6
+
+        int contor2 = 10;
+        int result2 = ++contor2; //11
+        System.out.println(contor); //11
+        //identic pentru --
+
+        String multilineString = "Deprecated\t Gradle features" +
+                " were used in \nthis build, making" +
+                " it incompatible with Gradle 7.0.";
+        System.out.println("multilineString = " + multilineString);
+
+    }
+
+}
